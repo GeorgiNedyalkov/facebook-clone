e87ac349c2d94a319476f95e6d9a95d7b58f2ff6
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2022-08-24 16:46:16 +0300

    Created the form in the feed section

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar } from "@mui/material"
 import { InsertEmoticon, PhotoLibrary, Videocam } from '@mui/icons-material';
 import './MessageSender.css'
 
 function MessageSender() {
+  const [input, setInput] = useState();
   const handleSubmit = (event) => {
         event.preventDefault();
     }
@@ -17,7 +18,7 @@ function MessageSender() {
             placeholder={`What's on your mind?`}
             className="messageSender__input"
             />
-            <input placeholder='image URL (Optional)' />
+            <input className="messageSender__input" placeholder='image URL (Optional)' />
             <button onClick={handleSubmit} type="submit">
                 Hidden Sumbit
             </button>
@@ -27,8 +28,12 @@ function MessageSender() {
             <div className='messageSender__option'>
                 <Videocam style={{ color: 'red' }} />
                 <h3>Live Video</h3>
+            </div>
+            <div className='messageSender__option'>
                 <PhotoLibrary style={{ color: 'green' }} />
                 <h3>Photo/Video</h3>
+            </div>
+            <div className='messageSender__option'>
                 <InsertEmoticon style={{ color: 'orange' }} />
                 <h3>Feeling/Activity</h3>
             </div>
